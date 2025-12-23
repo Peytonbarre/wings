a442238995d9f7e0112046d7338708a9f84edf72
Peytonbarre <peypeyozzy@gmail.com>
2025-12-22 22:41:00 -0600

    removed unused imports

--- src/main/java/com/wings/repository/impl/SpottedBirdRepositoryImpl.java
+++ src/main/java/com/wings/repository/impl/SpottedBirdRepositoryImpl.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
--- src/main/java/com/wings/service/BirdingService.java
+++ src/main/java/com/wings/service/BirdingService.java
@@ -1,10 +1,8 @@
 package com.wings.service;
 
-import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
